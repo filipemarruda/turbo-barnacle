99691d9b173c7a9a18767c7ae854ed0c3656aa0f
Filipe Arruda <filipemarruda@gmail.com>
2023-10-09 15:30:19 -0300

    adding pipeline build steps

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,5 +1,5 @@
 pipeline {
-    agent { docker 'maven:3.9.3-eclipse-temurin-17' }
+    agent { dockerContainer 'maven:3.9.3-eclipse-temurin-17' }
     stages {
         stage('Example Build') {
             steps {
