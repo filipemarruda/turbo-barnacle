5df0bd0fa8c67a0e08f9c860d682bb07a4d157d2
Filipe Arruda <filipemarruda@gmail.com>
2023-10-09 15:24:52 -0300

    adding pipeline build steps

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,7 +1,10 @@
-agent {
-    docker {
-        image 'maven:3.9.3-eclipse-temurin-17'
-        label 'my-defined-label'
-        args  '-v /tmp:/tmp'
+pipeline {
+    agent { docker 'maven:3.9.3-eclipse-temurin-17' }
+    stages {
+        stage('Example Build') {
+            steps {
+                sh 'mvn -B clean verify'
+            }
+        }
     }
 }
